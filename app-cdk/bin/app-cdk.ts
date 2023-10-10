468bbf4e973f5b4c07d46edb6447941e8272b6e5
Jennifer Wu <jenniwu@users.noreply.github.com>
2023-10-10 08:17:08 -0700

    add docker build and push

--- app-cdk/bin/app-cdk.ts
+++ app-cdk/bin/app-cdk.ts
@@ -1,10 +1,16 @@
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { AppCdkStack } from '../lib/app-cdk-stack';
-import { PipelineCdkStack } from '../lib/pipeline-cdk-stack'
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { AppCdkStack } from "../lib/app-cdk-stack";
+import { PipelineCdkStack } from "../lib/pipeline-cdk-stack";
+import { EcrCdkStack } from "../lib/ecr-cdk-stack";
 
 const app = new cdk.App();
 
-new AppCdkStack(app, 'AppCdkStack', {});
+const ecrCdkStack = new EcrCdkStack(app, "ecr-stack", {});
 
-new PipelineCdkStack(app, 'pipeline-stack', {});
+new AppCdkStack(app, "AppCdkStack", {});
+
+const pipelineCdkStack = new PipelineCdkStack(app, "pipeline-stack", {
+  ecrRepository: ecrCdkStack.repository,
+});
